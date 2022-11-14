a91292fe9a95ca73c9171b520dd2ee68b9d6db1c
Aitana Shough <aeshough@gmail.com>
2022-11-14 14:37:39 -0800

    add test to check for surpassed lifespan on jupiter

--- __test__/planetage.test.js
+++ __test__/planetage.test.js
@@ -76,5 +76,9 @@ describe('User', () => {
 
   test("should return # of years surpassed on Mars if user's age is over their avg life expectancy", () => {
     expect(joe.getYearsLeft('mars')).toEqual(8);
-  })
+  });
+
+  test("should return # of years surpassed on Jupiter if user's age is over their avg life expectancy", () => {
+    expect(joe.getYearsLeft('jupiter')).toEqual(47);
+  });
 })

f669311a0ec57f976a6249aadf2a3a00054fc261
Aitana Shough <aeshough@gmail.com>
2022-11-14 08:36:32 -0800

    refactor test and logic, updating class name to 'User'

--- __test__/planetage.test.js
+++ __test__/planetage.test.js
@@ -1,10 +1,10 @@
-import {UserAge} from '../src/js/planetage.js'
+import {User} from '../src/js/planetage.js'
 
-describe('UserAge', () => {
+describe('User', () => {
   let aitana;
 
   test("should create an object constructor with a user's name and age", () => {
-  aitana = new UserAge("Aitana", 25);
+  aitana = new User("Aitana", 25);
   expect(aitana).toEqual({name: "Aitana", age: 25});
   })
 })
--- src/js/planetage.js
+++ src/js/planetage.js
@@ -1,4 +1,4 @@
-export class UserAge {
+export class User {
   constructor(name, age) {
     this.name = name;
     this.age = age;

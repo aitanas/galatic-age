fc75767e2b43c7893a238aa547c25d786c32bb09
Aitana Shough <aeshough@gmail.com>
2022-11-14 16:09:11 -0800

    WIP: js UI

--- src/index.js
+++ src/index.js
@@ -4,17 +4,29 @@ import './css/styles.css';
 import { User } from './js/planetage.js';
 
 function handleResults(e) {
-  e.preventDefault;
+  e.preventDefault();
   const nameInput = document.getElementById("name").value;
   const ageInput = document.getElementById("age").value;
   const expectancyInput = document.getElementById("expectancy").value;
-  const planetInput = document.querySelector("input[name='planet']:checked").value;
+  const planetInput = document.querySelector("input[name='planet']:checked");
+  let planetInputArr = Array.from(planetInput);
+  const planetArr = [];
+  planetInputArr.forEach(function(element) {
+    planetArr.push(element.value);
+  });
+
+  const planet = String(planetArr);
+
   const paragraph = document.createElement("p");
   const h2 = document.createElement("h2");
 
   let user = new User(nameInput, ageInput, expectancyInput);
 
-  h2.append("Order placed!");
-  paragraph.append(`Hello ${nameInput}! Your age on ${planetInput} is ${user.getAge(planetInput)} years. Your estimated time left is ${user.getYearsLeft(planetInput)} ${planetInput} years. Thank you!`);
+  h2.append("Age calculated!");
+  paragraph.append(`Hello ${nameInput}! Your age on ${planet} is ${user.getAge(planet)} years. Your estimated time left is ${user.getYearsLeft(planet)} ${planet} years. Thank you!`);
   document.body.append(h2, paragraph);
 }
+
+window.addEventListener("load", function () {
+  document.querySelector("form#form").addEventListener("submit", handleResults);
+});
